03e6a253d49e2f220b48edf487291b51cb7b79f6
Khoa Hoang <khoahoang1698@gmail.com>
2025-03-13 14:43:32 -0700

    database

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,20 +1,14 @@
-#create a database and use mysql as engine, version 8.0.40,
-#this database main region is in us-west-1, then replica to us-east-1
-
+###database
 resource "aws_db_instance" "my_db" {
   allocated_storage = 20
-  db_name = "my-db"
+  db_name = "mydb"
   engine = "mysql"
   engine_version = "8.0.40"
-  instance_class = "db.t4g.micro"
+  instance_class = "db.t3.micro"
   username = "admin"
   password = "password"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot = true
-
-  #setting connectivity
-
-  #vpc
-  
-
+  vpc_security_group_ids = [ aws_security_group.my_db_sg.id ]
+  publicly_accessible = true
 }
