9c370e4fe9fff08614a9eddc32b6e9caab6ec91b
Khoa Hoang <khoahoang1698@gmail.com>
2025-03-13 15:35:08 -0700

    add database

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,5 +1,18 @@
+#subnet group
+resource "aws_db_subnet_group" "my_db_subnet_group" {
+  provider = aws
+  name = "my-db-subnet-group"
+  subnet_ids = [ aws_subnet.my_db_subnet_1.id, aws_subnet.my_db_subnet_2.id ]
+
+  tags = {
+    Name = "my-db-subnet-group"
+  }
+}
+
 ###database
 resource "aws_db_instance" "my_db" {
+  identifier = "my-db"
+  provider = aws
   allocated_storage = 20
   db_name = "mydb"
   engine = "mysql"
@@ -10,5 +23,8 @@ resource "aws_db_instance" "my_db" {
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot = true
   vpc_security_group_ids = [ aws_security_group.my_db_sg.id ]
+  db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
   publicly_accessible = true
+  depends_on = [ aws_security_group.my_db_sg ]
+  
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,8 @@ resource "aws_vpc" "my_db_vpc" {
   provider = aws
   cidr_block = "10.0.0.0/16"
 
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
     Name = "my-db-vpc"
   }
